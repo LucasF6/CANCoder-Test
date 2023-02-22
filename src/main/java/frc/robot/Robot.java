719ba0f1152e73179b5fba47d47bf2a205282259
ROBOTICS-02 <ROBOTICS-02@sbhsd.k12.ca.us>
2023-02-22 11:02:18 -0800

    added set position to absolute

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,12 +4,20 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.sensors.AbsoluteSensorRange;
 import com.ctre.phoenix.sensors.CANCoder;
 import com.ctre.phoenix.sensors.CANCoderConfiguration;
+import com.ctre.phoenix.sensors.SensorInitializationStrategy;
 import com.ctre.phoenix.sensors.SensorTimeBase;
+import com.ctre.phoenix.sensors.SensorVelocityMeasPeriod;
 import com.ctre.phoenix.sensors.WPI_CANCoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -18,9 +26,13 @@ import edu.wpi.first.wpilibj.TimedRobot;
  * project.
  */
 public class Robot extends TimedRobot {
+  Joystick m_joystick = new Joystick(0);
   public static final int ENCODER_ID = 3;
   public static final int MOTOR_ID = 1;
   WPI_CANCoder m_encoder;
+  CANSparkMax m_motor;
+  PIDController m_pid = new PIDController(0, 0, 0);
+  double m_setpoint = 0;
   
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -29,13 +41,17 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     m_encoder = new WPI_CANCoder(ENCODER_ID);
+    m_motor = new CANSparkMax(MOTOR_ID, MotorType.kBrushless);
     CANCoderConfiguration config = new CANCoderConfiguration();
-    config.sensorCoefficient = 1/4096;
-    config.unitString = "rotatioms";
+    config.sensorCoefficient = 360/4096;
+    config.unitString = "degrees";
     config.sensorTimeBase = SensorTimeBase.PerSecond;
-    System.out.println(config.sensorCoefficient * 4096);
+    config.velocityMeasurementPeriod = SensorVelocityMeasPeriod.Period_100Ms;
+    config.absoluteSensorRange = AbsoluteSensorRange.Signed_PlusMinus180;
+    config.sensorDirection = false;
+    config.initializationStrategy = SensorInitializationStrategy.BootToAbsolutePosition;
     m_encoder.configAllSettings(config);
-    // c * 4096 = 1
+    m_pid.enableContinuousInput(-180, 180);
   }
 
   @Override
@@ -51,7 +67,36 @@ public class Robot extends TimedRobot {
   public void teleopInit() {}
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    if (m_joystick.getTriggerPressed()) {
+      m_setpoint = 0;
+    }
+    if (m_joystick.getTopPressed()) {
+      m_setpoint = 90;
+    }
+    if (m_joystick.getRawButtonPressed(3)) {
+      m_setpoint = -180;
+    }
+    if (m_joystick.getRawButtonPressed(4)) {
+      m_setpoint = -90;
+    }
+    if (m_joystick.getRawButtonPressed(5)) {
+      m_encoder.setPosition(90);
+    }
+    if (m_joystick.getRawButtonPressed(6)) {
+      m_encoder.setPosition(-90);
+    }
+    if (m_joystick.getRawButtonPressed(7)) {
+      m_encoder.setPositionToAbsolute();
+    }
+    
+    m_motor.set(m_pid.calculate(m_encoder.getAbsolutePosition(), m_setpoint));
+    SmartDashboard.putNumber("relative position", m_encoder.getPosition());
+    SmartDashboard.putNumber("Absolute Position", m_encoder.getAbsolutePosition());
+    SmartDashboard.putNumber("Difference", m_encoder.getPosition() - m_encoder.getAbsolutePosition());
+    SmartDashboard.putNumber("velocity", m_encoder.getVelocity());
+    SmartDashboard.putNumber("bus voltage", m_encoder.getBusVoltage());
+  }
 
   @Override
   public void disabledInit() {}
